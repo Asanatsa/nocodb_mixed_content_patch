3b0e6d96c3fef8c25a11f26738add87c76de3648
Asanatsa <me@asanatsa.cc>
2024-01-31 22:33:42 +0800

    init

---
+++ main.js
@@ -0,0 +1,24 @@
+/**
+ * for fix "mixed context" bug
+ * @author Asanatsa
+ */
+
+
+(function () {
+
+    let callback = function () {
+        let imgEle = document.getElementsByTagName("img");
+        imgEle.forEach(e => {
+            if (e.src.search(/^http:\/\//i) !== -1){
+                e.src = e.src.replace("http://", "https://");
+            }
+            if (typeof e.srcset === "string" && e.srcset.search(/http:\/\//i) !== -1) {
+                e.srcset = e.srcset.replace("http://", "https://");
+                console.log(e)
+            }
+        });
+    }
+
+    document.body.addEventListener("DOMNodeInserted", callback);
+    document.body.addEventListener("DOMSubtreeModified", callback);
+})();
